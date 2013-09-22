09addfe4c206cffd6dad780e38db34cbff36ec99
Chris Applegate <github@qwghlm.co.uk>
2013-09-22 03:06:31 +0100

    Merge index and add, add in top 5

--- articles/views.py
+++ articles/views.py
@@ -2,42 +2,42 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 
 from articles.models import CIFArticle, CIFArticleForm
 
 def index(request):
-    form = CIFArticleForm()
-    return render(request, 'articles/index.html', { 'form' : form })
-
-def add(request):
-
-    if not request.POST:
-        return redirect("/")
 
     article = None
+    if request.POST:
+        try:
+            article = CIFArticle.objects.get(url=request.POST.get('url'))
 
-    try:
-        article = CIFArticle.objects.get(url=request.POST.get('url'))
+        except CIFArticle.DoesNotExist:
+            form = CIFArticleForm(request.POST)
 
-    except CIFArticle.DoesNotExist:
-        form = CIFArticleForm(request.POST)
-
-        # FIXME - incorporate updating & validation into Django's own validation?
-        if (form.is_valid()):            
-            try:
-                article = form.save(commit=False)
-                article.measure_ego()
-                article.save()
-            except ValueError:
-                article = None
+            # FIXME - incorporate updating & validation into Django's own validation?
+            if (form.is_valid()):            
+                try:
+                    article = form.save(commit=False)
+                    article.measure_ego()
+                    article.save()
+                except ValueError:
+                    article = None
+    else:
+        form = CIFArticleForm()
 
     if article:
-        return redirect("/" + str(article.id))
-    else:
-        return render(request, 'articles/index.html', { 'form' : form })
+        return redirect(reverse("articles:detail", args=(article.id,)))
 
+    else:
+        top_articles = CIFArticle.objects.order_by('-score')[:5]
+        return render(request, 'articles/index.html', {
+            'form' : form ,
+            'top_articles' : top_articles
+        })
 
 def detail(request, article_id):
-    article = get_object_or_404(CIFArticle, id=article_id)
+    article = get_object_or_404(CIFArticle, id=article_id) # TODO What does the 404 look like?
     return render(request, 'articles/detail.html', {'article' : article})
 
