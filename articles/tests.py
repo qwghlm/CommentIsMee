b15d88ffec29e50e7c34813e03acf514607afa4d
Chris Applegate <github@qwghlm.co.uk>
2013-09-22 18:58:01 +0100

    Some very basic tests

--- articles/tests.py
+++ articles/tests.py
@@ -5,12 +5,17 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
+client = Client()
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def test_home_view(self):
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_view(self):
+        response = client.get('/1/')
+        self.assertEqual(response.status_code, 200)
+
