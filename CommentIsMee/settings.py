155698e97bfde2b853eb28d2d4b731e7da279c2d
Chris Applegate <qwghlm@users.noreply.github.com>
2013-09-24 12:43:00 +0100

    No need for bootstrap_toolkit, add in extra domain

--- CommentIsMee/settings.py
+++ CommentIsMee/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['django.local']
+ALLOWED_HOSTS = ['django.local', 'commentismee.qwghlm.co.uk']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -124,9 +124,13 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'articles',
-    'bootstrap_toolkit',
+    
+    # Humanize
     'django.contrib.humanize'
+    
+    # Articles
+    'articles',
+
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
