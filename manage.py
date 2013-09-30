0afcc778d162ecf46a5318b54141a8e047e650de
Chris Applegate <github@qwghlm.co.uk>
2013-09-30 12:04:21 +0100

    Tweaks for production

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CommentIsMee.settings_dev")
+    DEBUG = (sys.argv[1] == 'runserver')
+    if DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CommentIsMee.settings_dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CommentIsMee.settings_prod")
 
     from django.core.management import execute_from_command_line
 
